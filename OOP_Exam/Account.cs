e85ecd2f14f2595a5a5fd2f1b4c222b46c7c9fff
Cormac Mc Manus <S00197425@mail.itsligo.ie>
2021-01-11 10:41:51 +0000

    Sill need to implement buttoon functionality

---
+++ OOP_Exam/Account.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exam
+{
+    abstract class Account
+    {
+        public string FirstName { get; set; }
+        public string SurName { get; set; }
+        public decimal Balance { get; set; }
+        public DateTime InterestDate { get; set; }
+
+        abstract public decimal CalculateInterest();
+
+        public decimal Deposit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal WithDraw()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+    /******************** Current Acoount *************************/
+    class CurrentAccount : Account
+    {
+        public decimal InterestRate = 0.03m;
+
+        public CurrentAccount(string firstname, string surname, decimal balance)
+        {
+            FirstName = firstname;
+            SurName = surname;
+            Balance = balance;
+
+        }
+
+        public override decimal CalculateInterest()
+        {
+            decimal interest = 0;
+            interest = Balance * InterestRate;
+            return interest;
+            InterestDate = DateTime.Now;
+        }
+    }
+
+
+
+
+
+
+
+    /******************** Savings Acoount *************************/
+    class SavingsAccount : Account
+    {
+        public decimal InterestRate = 0.06m;
+
+        public SavingsAccount(string firstname, string surname, decimal balance)
+        {
+            FirstName = firstname;
+            SurName = surname;
+            Balance = balance;
+
+        }
+
+        public override decimal CalculateInterest()
+        {
+            decimal interest = 0;
+            interest = Balance * InterestRate;
+            return interest;
+            InterestDate = DateTime.Now;
+            
+        }
+    }
+}
--- OOP_Exam/MainWindow.xaml.cs
+++ OOP_Exam/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,48 @@ namespace OOP_Exam
         public MainWindow()
         {
             InitializeComponent();
+        }
+        //Dummy data
+        CurrentAccount CA1 = new CurrentAccount("John", "Doe", 1295);
+        SavingsAccount SA1 = new SavingsAccount("Jane", "Doe", 1937);
+        CurrentAccount CA2 = new CurrentAccount("Joe", "Smith", 1375);
+        SavingsAccount SA2 = new SavingsAccount("Jess", "Murphy", 1937);
+
+        ObservableCollection<Account> AccountCollection = new ObservableCollection<Account>();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            //Dummy data added to collection to be viewed in list.
+            AccountCollection.Add(CA1);
+            AccountCollection.Add(SA1);
+            AccountCollection.Add(CA2);
+            AccountCollection.Add(SA2);
+
+            lbxAccountNames.ItemsSource = AccountCollection;
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //find selected account
+            Account selectedAccount = lbxAccountNames.SelectedItem as Account;
+            //ensure selection is not null
+            if (selectedAccount != null)
+            {
+                //populate text boxes
+                fNameBox.Text = selectedAccount.FirstName;
+                sNameBox.Text = selectedAccount.SurName;
+                balanceBox.Text = selectedAccount.Balance.ToString();
+                if (selectedAccount is CurrentAccount)
+                {
+                    accTypeBox.Text = "Current Account";
+                }
+                else if (selectedAccount is SavingsAccount)
+                {
+                    accTypeBox.Text = "Savings Account";
+                }
+                interestBox.Text = selectedAccount.InterestDate.ToString();
+
+            }
         }
     }
 }
